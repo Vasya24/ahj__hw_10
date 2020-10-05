7425e44c4b1e32cc822232830b1b7d6c03bbc28a
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-06 00:13:54 +0300

    Нужно довести до ума Date, а также настроить модалочку

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,33 @@
-console.log('app.js bundled');
+const chat = document.querySelector('.chat');
+const chatMsgs = document.querySelector('.chat_messages');
+const input = document.querySelector('.input');
+const date = new Date();
+console.log(date.getFullYear())
 
-if (navigator.geolocation) {
-  console.log('geo');
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-      const { latitude, longitude, accuracy } = position.coords;
-      console.log(`lat: ${latitude}, lng: ${longitude}, acc: ${accuracy}`);
-    }, (error) => {
-      console.log(error);
-    },
-  );
-}
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position);
+          const { latitude, longitude, accuracy } = position.coords;
+          chatMsgs.insertAdjacentHTML('afterbegin', `
+          <div class="message_box">
+          <div class="text_geo_box">
+          <p class="msg_txt">${input.value}</p>
+          <p class="msg_geo">${latitude}, ${longitude}, ${accuracy}</p>
+          </div>
+          <div class="date_time_box">
+            ${date.getDate()}
+          </div>
+          </div>
+          `)
+          input.value = ''         
+        }, (error) => {
+          console.log(error);
+        },
+      );
+    }
+  }
+})
+
