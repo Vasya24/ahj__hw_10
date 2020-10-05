26811aa3f9d3316fbcaadb3a47cbb5c252a154e2
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-05 19:20:44 +0300

    Поправил линт

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+console.log('app.js bundled');
+
+if (navigator.geolocation) {
+  console.log('geo');
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+      const { latitude, longitude, accuracy } = position.coords;
+      console.log(`lat: ${latitude}, lng: ${longitude}, acc: ${accuracy}`);
+    }, (error) => {
+      console.log(error);
+    },
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,47 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    devtool: 'source-map',
-    devServer: {
-        port: 4242,
+  devtool: 'source-map',
+  devServer: {
+    port: 4242,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+      }],
     },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                }, ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpg)$/,
-                use: [{ loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'img/', }, }, { loader: 'image-webpack-loader' }],
-            },
-        ],
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader, 'css-loader',
+      ],
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
+    {
+      test: /\.(png|jpg)$/,
+      use: [{ loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'img/' } }, { loader: 'image-webpack-loader' }],
+    },
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
